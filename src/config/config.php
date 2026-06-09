5f07d767ecb7d72e2d0b8f2ebd9525443c6b238a
kadokurwa <kadokurwa22@gmail.com>
2026-06-09 08:50:33 +0300

    Create config.php

---
+++ src/config/config.php
@@ -0,0 +1,20 @@
+<?php
+// Application configuration
+if (!defined('APP_NAME')) {
+    define('APP_NAME', 'Mzumbe University Online Registration');
+}
+if (!defined('DB_NAME')) {
+    define('DB_NAME', 'mzumbe_courses');
+}
+if (!defined('DB_HOST')) {
+    define('DB_HOST', 'localhost:3306');
+}
+if (!defined('DB_USER')) {
+    define('DB_USER', 'root');
+}
+if (!defined('DB_PASSWORD')) {
+    define('DB_PASSWORD', '');
+}
+if (!defined('BASE_URL')) {
+    define('BASE_URL', '/ONLINE_REGISTRATION');
+}
