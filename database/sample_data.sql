12ce5e2a7116d36f37976f54638ce2328e82afe7
kadokurwa <kadokurwa22@gmail.com>
2026-06-09 08:47:52 +0300

    Create sample_data.sql

---
+++ database/sample_data.sql
@@ -0,0 +1,10 @@
+USE mzumbe_courses;
+
+INSERT INTO courses (course_code, course_title, credit_hours, semester, department, max_enrollment) VALUES
+('CSE101', 'Introduction to Computer Science', 3, 'Semester 1', 'Computer Science', 120),
+('CSE102', 'Programming Fundamentals', 4, 'Semester 1', 'Computer Science', 100),
+('BUS201', 'Principles of Management', 3, 'Semester 1', 'Business Administration', 120),
+('MTH101', 'Calculus I', 4, 'Semester 1', 'Mathematics', 120),
+('ENG101', 'Academic Writing', 2, 'Semester 1', 'General Studies', 150);
+
+-- No sample students are inserted here. Use the registration form to create student accounts.
